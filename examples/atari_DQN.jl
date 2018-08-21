11a1dceffe38d1f9bb685987c519220a5479d3e9
root <root@iccluster028.iccluster.epfl.ch>
2018-08-21 21:35:22 +0200

    change parameters

--- examples/atari_DQN.jl
+++ examples/atari_DQN.jl
@@ -1,39 +1,46 @@
 using RLEnvAtari, Flux, GR, JLD2
-import ArcadeLearningEnvironment: getMinimalActionSet, getLegalActionSet
-const withgpu = false
+using Compat: @info
+const withgpu = true
+const game = "alien"
+const gpudevice = 1
+
 if withgpu 
+    using CUDAnative
+    device!(gpudevice)
     using CuArrays
     const inputdtype = Float32
 else
     const inputdtype = Float64
 end
-env = AtariEnv("breakout")
-model = Chain(x -> x./inputdtype(255), Conv((8, 8), 4 => 32, relu, stride = (4, 4)), 
-                         Conv((4, 4), 32 => 64, relu, stride = (2, 2)),
-                         Conv((3, 3), 64 => 64, relu),
+env = AtariEnv(game)
+model = Chain(x -> x./inputdtype(255), Conv((8, 8), 4 => 16, relu, stride = (4, 4)), 
+                         Conv((4, 4), 16 => 32, relu, stride = (2, 2)),
                          x -> reshape(x, :, size(x, 4)),
-                         Dense(3136, 512, relu), 
-                         Dense(512, length(env.actions)));
-learner = DQN(model, opttype = x -> Flux.RMSProp(x, .00025, ρ = .95, ϵ = .01), 
-              loss = huberloss,
-              updatetargetevery = 10^4, replaysize = 10^6, nmarkov = 4,
-              startlearningat = 50000);
+                         Dense(2592, 256, relu), 
+                         Dense(256, length(env.actions)));
+learner = DQN(model, opttype = x -> Flux.ADAM(x, .0001), 
+              loss = huberloss, doubledqn = true,
+              updatetargetevery = 2500, nsteps = 10,
+              updateevery = 4, replaysize = 10^6, nmarkov = 4,
+              startlearningat = 200000);
 x = RLSetup(learner, 
             env,
             ConstantNumberSteps(2000),
             preprocessor = AtariPreprocessor(gpu=withgpu, outdim = (84, 84)),
             callbacks = [Visualize(wait = 0)])
 GR.inline("mov")
-beginprint("before.mov")
+beginprint("before$game.mov")
 run!(x)
 endprint()
+learner.t = 0
 x.callbacks = [Progress(5*10^2), EvaluationPerEpisode(TotalReward()),
-               LinearDecreaseEpsilon(5 * 10^4, 10^6, 1, .1)];
+               LinearDecreaseEpsilon(5 * 10^4, 10^6, 1, .01)];
 x.stoppingcriterion = ConstantNumberSteps(4 * 10^6)
+@info "start learning."
 @time learn!(x)
-@save "model.jld2" model
+@save "model$game.jld2" model
 x.callbacks = [Visualize(wait = 0)]
 x.stoppingcriterion = ConstantNumberSteps(2000)
-beginprint("after.mov")
+beginprint("after$game.mov")
 run!(x)
 endprint()
