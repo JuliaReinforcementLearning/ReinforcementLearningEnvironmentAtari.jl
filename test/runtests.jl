c19cb3374346876157289b3ab304c28d55ff7bf9
Jun Tian <find_my_way@foxmail.com>
2018-09-06 15:02:43 +0800

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,5 @@ using Test
 import ReinforcementLearningEnvironmentAtari: reset!, interact!, getstate
 env = AtariEnv("pong")
 reset!(env)
-@test typeof(interact!(env, 1)) == Tuple{Array{UInt8, 1}, Int32, Bool}
-@test typeof(getstate(env)) == Tuple{Array{UInt8, 1}, Bool}
+@test typeof(interact!(env, 1)) == NamedTuple{(:observation, :reward, :isdone), Tuple{Array{UInt8, 1}, Int32, Bool}}
+@test typeof(getstate(env)) == NamedTuple{(:observation, :isdone), Tuple{Array{UInt8, 1}, Bool}}
