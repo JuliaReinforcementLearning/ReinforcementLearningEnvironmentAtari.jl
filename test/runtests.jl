cf0c5c13d2efdd688ce7743d840598f40f86a608
Johanni Brea <jbrea@users.noreply.github.com>
2019-02-15 17:13:20 +0100

    test settings

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,8 @@
 using ReinforcementLearningEnvironmentAtari, Test, ReinforcementLearningBase
 
 test_envinterface(AtariEnv("pong"))
+
+env = AtariEnv("pong", frame_skip = 10000)
+@test ReinforcementLearningEnvironmentAtari.interact!(env, 1).isdone == true
+env = AtariEnv("pong", frame_skip = 1)
+@test ReinforcementLearningEnvironmentAtari.interact!(env, 1).isdone == false
